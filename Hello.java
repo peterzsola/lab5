ebb37b8de824a468f9fc57a70410acd01be1cde8
peterzsola <peterz@go.stockton.edu>
2020-11-29 21:44:48 -0500

    Revert "Oops, we didn't want this commit"
    This reverts commit 98d0a4a2f65cd52329f81d99e9734a20983cb341.

--- Hello.java
+++ Hello.java
@@ -2,7 +2,6 @@ public class Hello
 {
  public static void main( String argv[] )
  {
- // This is an unwanted but staged comment
  String name = "World";
  if ( argv.length != 0 )
  {
@@ -11,3 +10,4 @@ public class Hello
  System.out.println( "Hello, " + name + "!" );
  }
 }
+
